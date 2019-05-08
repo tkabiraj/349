bae0eadc5a27087cd9aaebf29b231eb0c9c4b081
tkabiraj <tkabiraj@unix1.csc.calpoly.edu>
2019-05-08 09:44:51 -0700

    tried an algorithm

--- Project3/GameProblem.java
+++ Project3/GameProblem.java
@@ -1,30 +1,61 @@
+import java.util.Scanner;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.io.*;
+
 public class GameProblem {
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws IOException{
       Scanner scan = new Scanner(System.in);
       System.out.println("Input file name");
       String str = scan.nextLine();
       Path filePath = Paths.get(str);
       Scanner scanner = new Scanner(filePath);
       
-      n = scanner.nextInt();
-      m = scanner.nextInt();
+      int n = scanner.nextInt();
+      int m = scanner.nextInt();
       int[][] A = new int[n][m];
       for(int i = 0; i < A.length; i++) {
          for(int j = 0; j < A[0].length; j++)
             A[i][j] = scanner.nextInt();
       }
-      int[][] C = game(n, m, A);
+      game(n, m, A);
    }
 
    public static void game(int n, int m, int[][] A) {
       int[][] S = new int[n+1][m+1];
+      char[][] R = new char[n+1][m+1];
+      int maxSum = A[n-1][m-1], maxI = 1, maxJ = 1;
 
-      for(int i = 1; i < S.length; i ++) {
-         for(int j = 1; j < S[0].length; j++) {
-
+      for(int i = S.length - 2; i > 0; i --) {
+         for(int j = S[0].length - 2; j > 0; j--) {
+            if(A[i][j] + S[i + 1][j] >= S[i][j + 1]) {
+               S[i][j] = A[i][j] + S[i + 1][j];
+               R[i][j] = 'd';
+            }else{
+               S[i][j] = A[i][j] + S[i][j + 1];
+               R[i][j] = 'r';
+            }
+            if(S[i][j] > maxSum) {
+               maxSum = S[i][j];
+               maxI = i;
+               maxJ = j;
+            }
          }
       }
+      System.out.println("Best score: " + maxSum);
+      printRoute(R, maxI, maxJ);
+   }
 
+   private static void printRoute(char[][] R, int i, int j) {
+      if(i == R.length - 1 || j == R[0].length - 1)
+         System.out.println("exit");
+      else {
+         System.out.print("[" + (i + 1) + "," + (j + 1) + "] to ");
+         if(R[i][j] == 'r')
+            printRoute(R, i, j + 1);
+         else
+            printRoute(R, i + 1, j);
+      }
    }
 }
